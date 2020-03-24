720cb8b6b3cec94597ed99d31ed546eb490b138b
JoshMA503 <josuealeman2003@gmail.com>
2020-03-23 23:52:35 -0600

    fv3

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors'); const app = express()
+const bodyparser = require('body-parser'); 
+const port = process.env.PORT || 3000
+
+//middlewares
+app.use(cors())
+app.use(bodyparser.json()); 
+//parser responses
+app.use('/',(req, res)=>{ res.send('hello') })
+app.use(bodyparser.urlencoded({     
+    extended: true
+}))
+
+//init app
+app.listen(port, function () {
+    console.log('app listening on port 3000!');
+    console.log('listo')
+});
+
