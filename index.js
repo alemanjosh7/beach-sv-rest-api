cf565b8a94611207ac508d12cf0251358c1cf6e5
JoshMA503 <josuealeman2003@gmail.com>
2020-03-23 23:54:39 -0600

    fv4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(bodyparser.json()); 
 //parser responses
-app.use('/',(req, res)=>{ res.send('hello') })
+app.use('/',(req, res)=>{ res.send('hello from heroku') })
 app.use(bodyparser.urlencoded({     
     extended: true
 }))
